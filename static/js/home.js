74893fc2ed453f22f72c40c466b934795c76cac4
Ben Prothe <beprothe@gmail.com>
2020-03-12 22:26:55 -0400

    connect JS and started page interactions

---
+++ static/js/home.js
@@ -0,0 +1,37 @@
+// -----------------------HELPER FUNCTIONS---------------------
+function dQS (elementID) {
+  return document.querySelector(elementID)
+}
+
+function dQSA (elementClass) {
+  return document.querySelectorAll(elementClass)
+}
+
+function hide (element) {
+  element.classList.add('dn')
+}
+
+function show (element) {
+  element.classList.remove('dn')
+}
+
+// -------------------------BASIC HIDE AND SHOW--------------------
+
+function pageRun () {
+  const buttons = dQSA('.col-butt')
+  for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+      const formQuery = event.target.dataset.form
+      const forms = dQSA('.col-form')
+      for (const form of forms) {
+        if (form.id === formQuery) {
+          show(form)
+        } else {
+          hide(form)
+        }
+      }
+    })
+  }
+}
+
+pageRun()
