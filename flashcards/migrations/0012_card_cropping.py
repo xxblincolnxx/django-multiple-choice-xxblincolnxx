bab576819c27a64d0ef68153f7b416fb5ac7fb02
Ben Prothe <beprothe@gmail.com>
2020-03-18 17:02:24 -0400

    moar migration tweaks for heroku

--- flashcards/migrations/0012_card_cropping.py
+++ flashcards/migrations/0012_card_cropping.py
@@ -4,16 +4,16 @@ from django.db import migrations
 # import image_cropping.fields
 
 
-class Migration(migrations.Migration):
+# class Migration(migrations.Migration):
 
-    dependencies = [
-        ('flashcards', '0011_auto_20200318_1702'),
-    ]
+#     dependencies = [
+#         ('flashcards', '0011_auto_20200318_1702'),
+#     ]
 
-    operations = [
-        migrations.AddField(
-            model_name='card',
-            name='cropping',
-            field=image_cropping.fields.ImageRatioField('figure_raw', '200x400', adapt_rotation=False, allow_fullsize=False, free_crop=False, help_text=None, hide_image_field=False, size_warning=False, verbose_name='cropping'),
-        ),
-    ]
+#     operations = [
+#         migrations.AddField(
+#             model_name='card',
+#             name='cropping',
+#             field=image_cropping.fields.ImageRatioField('figure_raw', '200x400', adapt_rotation=False, allow_fullsize=False, free_crop=False, help_text=None, hide_image_field=False, size_warning=False, verbose_name='cropping'),
+#         ),
+#     ]
